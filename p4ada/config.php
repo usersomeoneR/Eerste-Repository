0ea28829ede2dc59f849a5876797cc9c1b28a2b2
usersomeoneR <61309701+usersomeoneR@users.noreply.github.com>
2020-05-14 15:22:39 +0200

    Het eerste project.

---
+++ p4ada/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localhost";
+$gebruiker = "root";
+$wachtwoord = "";
+$dbnaam = "mediamarkt";
+
+//Verbinding naar de mysql database
+$conn = new PDO("mysql:host=$host;dbname=$dbnaam",$gebruiker,$wachtwoord);
+
+
+?>
